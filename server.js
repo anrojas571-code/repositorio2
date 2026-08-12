d7b0d2a7228eaef0926dcb098c808b7d0de61055
anrojas571-code <anrojas571@gmail.com>
2026-08-12 10:55:00 -0400

    Correccion de servidor y panel admin

--- server.js
+++ server.js
@@ -119,6 +119,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'Public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor iniciado en puerto ${PORT}`);
 });
